529b0426f37d45e503e7339495fbff1d192fa17a
Johnny Corvisier <johnny.corvisier.lc@gmail.com>
2022-03-15 05:04:11 +0100

    Photographer page

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,30 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+	const { name, portrait, city, country, tagline, price, id } = data;
+	const picture = `assets/photographers/${portrait}`;
 
-    const picture = `assets/photographers/${portrait}`;
+	function getUserCardDOM() {
+		const a = document.createElement("a");
+		const article = document.createElement("article");
+		const img = document.createElement("img");
+		const h2 = document.createElement("h2");
+		const h3 = document.createElement("h3");
+		const h4 = document.createElement("h4");
+		const h5 = document.createElement("h5");
+		const link = "/photographer.html" + "?id=" + data.id;
+		img.setAttribute("src", picture);
+		a.setAttribute("href", link);
+		h2.textContent = name;
+		h3.textContent = city + ", " + country;
+		h4.textContent = tagline;
+		h5.textContent = price + "€/jour";
+		article.appendChild(a);
+		a.appendChild(img);
+		a.appendChild(h2);
+		a.appendChild(h3);
+		a.appendChild(h4);
+		a.appendChild(h5);
+		return article;
+	}
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+	return { name, portrait, city, country, tagline, price, id, getUserCardDOM };
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,168 @@
 //Mettre le code JavaScript lié à la page photographer.html
+async function getPhotographers() {
+	return fetch("data/photographers.json")
+		.then(function (res) {
+			if (res.ok) {
+				return res.json();
+			} else {
+				throw alert("Une erreur c'est produite.");
+			}
+		})
+		.then(function (data) {
+			return data;
+		})
+		.catch(function (err) {
+			throw err;
+		});
+}
+
+function getPhotographersId() {
+	return new URL(location.href).searchParams.get("id");
+}
+async function init() {
+	const photographers = await getPhotographers();
+	getUserHeader(photographers);
+	getUserMedia(photographers);
+}
+function getUserHeader(data) {
+	const photographerId = getPhotographersId();
+
+	for (i = 0; i < 6; i++) {
+		const photographer = data.photographers[i];
+		if (photographerId == photographer.id) {
+			const portrait = photographer.portrait;
+			const picture = `assets/photographers/${portrait}`;
+			const photographerHeader = document.querySelector(".photograph-header");
+			const photographerInfos = document.querySelector(".photograph-infos");
+			const h1 = document.createElement("h1");
+			const h2 = document.createElement("h2");
+			const h3 = document.createElement("h3");
+			const img = document.createElement("img");
+			img.setAttribute("src", picture);
+			img.setAttribute("id", "photograph-img");
+			h1.textContent = photographer.name;
+			h2.textContent = photographer.city + ", " + photographer.country;
+			h3.textContent = photographer.tagline;
+			photographerInfos.appendChild(h1);
+			photographerInfos.appendChild(h2);
+			photographerInfos.appendChild(h3);
+			photographerHeader.appendChild(img);
+		}
+	}
+}
+
+const lightbox = document.createElement("div");
+lightbox.id = "lightbox";
+document.body.appendChild(lightbox);
+
+lightbox.addEventListener("click", (e) => {
+	if (e.target !== e.currentTarget) return;
+	lightbox.classList.remove("active");
+});
+
+function getUserMedia(data) {
+	const photographerId = parseInt(getPhotographersId());
+
+	for (i = 0; i < 59; i++) {
+		const photographerMedia = data.media[i];
+		if (photographerId === photographerMedia.photographerId) {
+			if (photographerMedia.image) {
+				const gallery = document.querySelector(".gallery");
+				const galleryElt = document.createElement("div");
+				const eltInfos = document.createElement("div");
+				const likeContainer = document.createElement("div");
+				const image = photographerMedia.image;
+				const picture = `assets/media/${image}`;
+				const img = document.createElement("img");
+				const title = document.createElement("span");
+				const likes = document.createElement("span");
+				const heart = document.createElement("i");
+				img.setAttribute("src", picture);
+				img.setAttribute("id", "media");
+				galleryElt.setAttribute("id", "gallery-element");
+				eltInfos.setAttribute("class", "media-infos");
+				heart.setAttribute("class", "far fa-heart");
+				title.textContent = photographerMedia.title;
+				likes.textContent = photographerMedia.likes;
+				gallery.appendChild(galleryElt);
+				galleryElt.appendChild(img);
+				galleryElt.appendChild(eltInfos);
+				eltInfos.appendChild(title);
+				likeContainer.appendChild(likes);
+				likeContainer.appendChild(heart);
+				eltInfos.appendChild(likeContainer);
+				img.addEventListener("click", (e) => {
+					lightbox.classList.add("active");
+					const img = document.createElement("img");
+					const cross = document.createElement("img");
+					const chevron = document.createElement("img");
+					const chevronLeft = document.createElement("img");
+					chevron.src = "assets/icons/chevron.svg";
+					chevron.id = "chevron";
+					chevronLeft.src = "assets/icons/chevron.svg";
+					chevronLeft.id = "chevron-left";
+					cross.src = "assets/icons/close.svg";
+					cross.id = "cross";
+					cross.addEventListener("click", (e) => {
+						if (e.target !== e.currentTarget) return;
+						lightbox.classList.remove("active");
+					});
+					img.src = picture;
+					img.id = "lightbox-content";
+					while (lightbox.firstChild) {
+						lightbox.removeChild(lightbox.firstChild);
+					}
+					lightbox.appendChild(cross);
+					lightbox.appendChild(chevron);
+					lightbox.appendChild(chevronLeft);
+					lightbox.appendChild(img);
+				});
+			} else {
+				const gallery = document.querySelector(".gallery");
+				const galleryElt = document.createElement("div");
+				const eltInfos = document.createElement("div");
+				const likeContainer = document.createElement("div");
+				const video = photographerMedia.video;
+				const videoPath = `assets/media/${video}`;
+				const videoContainer = document.createElement("video");
+				const title = document.createElement("span");
+				const likes = document.createElement("span");
+				const heart = document.createElement("i");
+				videoContainer.setAttribute("src", videoPath);
+				videoContainer.setAttribute("id", "media");
+				galleryElt.setAttribute("id", "gallery-element");
+				eltInfos.setAttribute("class", "media-infos");
+				heart.setAttribute("class", "far fa-heart");
+				title.textContent = photographerMedia.title;
+				likes.textContent = photographerMedia.likes;
+				gallery.appendChild(galleryElt);
+				galleryElt.appendChild(videoContainer);
+				galleryElt.appendChild(eltInfos);
+				eltInfos.appendChild(title);
+				likeContainer.appendChild(likes);
+				likeContainer.appendChild(heart);
+				eltInfos.appendChild(likeContainer);
+				videoContainer.addEventListener("click", (e) => {
+					lightbox.classList.add("active");
+					const video = document.createElement("video");
+					const cross = document.createElement("img");
+					cross.src = "assets/icons/close.svg";
+					cross.id = "cross";
+					cross.addEventListener("click", (e) => {
+						if (e.target !== e.currentTarget) return;
+						lightbox.classList.remove("active");
+					});
+					video.src = videoPath;
+					video.id = "lightbox-content";
+					while (lightbox.firstChild) {
+						lightbox.removeChild(lightbox.firstChild);
+					}
+					lightbox.appendChild(cross);
+					lightbox.appendChild(video);
+				});
+			}
+		}
+	}
+}
+
+init();
